fe9d40e103aa2794ee0c2465d562657076af9bee
KDY <ehdussla98@gmail.com>
2026-09-03 09:48:03 +0900

    9/3

---
+++ src/main/java/com/springboot/ijib/IjibApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ijib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ijib/IjibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ijib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
