562a114da07a20049bd5638fc5aa445d92ff6f98
pbs <barkej9@gmail.com>
2026-09-15 17:09:34 +0900

    09/15 17:08 pass 인덱스 개편 작업 및 로그인 시 권한 수정
    - pass 인덱스 관련 개편
    - 로그인 시 DB 권한과 실제 권한 동기화

--- src/main/java/com/springboot/ijib/service/PassOrderESService.java
+++ src/main/java/com/springboot/ijib/service/PassOrderESService.java
@@ -10,10 +10,11 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.reindex.UpdateByQueryRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springboot.ijib.dao.IMemberDAO;
+import com.springboot.ijib.dto.MemberDTO;
 import com.springboot.ijib.dto.OrdersDTO;
 import com.springboot.ijib.dto.PassDTO;
 
@@ -22,47 +23,53 @@ public class PassOrderESService {
 	@Autowired
 	private RestHighLevelClient client;
 	
-	public void save(OrdersDTO odto, PassDTO pdto) {
-		try {
-			// 0. 주문이 제대로 됐는지, 주문 내역이 제대로 들어왔는지 확인
-			if (odto.getOno() == null) {
-                throw new IllegalStateException("주문 번호(ono)가 null입니다.");
-            }
-			
-			// 1. 인덱스 매핑의 yyyy-MM-dd HH:mm:ss 포맷에 맞춤
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String odate = LocalDateTime.now().format(formatter);
-
-            // 2. ES에 저장할 Map 생성
-            Map<String, Object> map = new HashMap<>();
-            map.put("pno", pdto.getPno());
-            map.put("pname", pdto.getPname());
-            map.put("pprice", odto.getOprice());
-            map.put("mno", odto.getMno());
-            map.put("odate", odate);
-            // ostatus 필드 추가
-            map.put("ostatus", "PAID");
-
-            // 3.ES에 저장
-            // 인덱스명은 "pass", 도큐먼트 ID는 주문 고유 번호(ono)
-            IndexRequest request = new IndexRequest("pass")
-                    .id(odto.getOno())
-                    .source(map);
-
-            client.index(request, RequestOptions.DEFAULT);
-
-            // 로그
-    		System.out.println("주문 번호: " + odto.getOno());
-
-        } catch (Exception e) {
-            // DB 결제는 이미 성공했으므로 ES 색인 실패 시 로그만 남김
-            System.err.println("Elasticsearch 색인 실패: " + e.getMessage());
-            e.printStackTrace();
+	@Autowired
+	private IMemberDAO mdao;
+	
+	public void save(OrdersDTO odto, PassDTO pdto) throws Exception {
+		// 0. 주문이 제대로 됐는지, 주문 내역이 제대로 들어왔는지 확인
+		if (odto.getOno() == null) {
+            throw new IllegalStateException("주문 번호(ono)가 null입니다.");
         }
+		
+		// 1-1. 인덱스 매핑의 yyyy-MM-dd HH:mm:ss 포맷에 맞춤
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime orderDateTime = (odto.getOdate() != null) ? odto.getOdate() : LocalDateTime.now();
+        String odate = orderDateTime.now().format(formatter);
+
+        // 1-2. member 정보 들고오기
+        MemberDTO member = mdao.memberView(odto.getMno());
+        
+        // 2. ES에 저장할 Map 생성
+        Map<String, Object> map = new HashMap<>();
+        map.put("pno", pdto.getPno());
+        map.put("pname", pdto.getPname());
+        map.put("pprice", odto.getOprice());
+        map.put("mno", odto.getMno());
+        map.put("odate", odate);
+        // ostatus 필드 추가
+        map.put("ostatus", "PAID");
+        // opayment 필드 추가
+        map.put("opayment", odto.getOpayment());
+        // mage 필드 추가
+        map.put("mage", member.getMage());
+        // mgender 필드 추가
+        map.put("mgender", member.getMgender());
+
+        // 3.ES에 저장
+        // 인덱스명은 "pass", 도큐먼트 ID는 주문 고유 번호(ono)
+        IndexRequest request = new IndexRequest("pass")
+                .id(odto.getOno())
+                .source(map);
+
+        client.index(request, RequestOptions.DEFAULT);
+
+        // 로그
+		System.out.println("주문 번호: " + odto.getOno());
 	}
 	
 	// 환불 성공 시 ES pass 인덱스 정보 동기화
-	public void refundStatusUpdate(String ono) {
+	public void refundStatusUpdate(String ono, String reason, String customText) {
 		try {
 			if (ono == null || ono.isBlank()) {
 				throw new IllegalStateException("주문 번호(ono)가 유효하지 않습니다.");
@@ -71,10 +78,16 @@ public class PassOrderESService {
 			// 1. 업데이트할 필드
 			Map<String, Object> map = new HashMap<>();
 			map.put("ostatus", "REFUND");
+			// 환불 사유 및 환불 상세(기타용) 추가
+			map.put("refund_reason", reason);
+			if ("기타".equals(reason)) {
+				map.put("refund_detail", customText);
+			}
 			
 			// 2. UpdateRequest 생성 (인덱스: pass, _id: ono)
 			UpdateRequest request = new UpdateRequest("pass", ono)
-					.doc(map);
+					.doc(map)
+					.docAsUpsert(true);
 			
 			// 3. ES 부분 갱신 요청
 			UpdateResponse response = client.update(request, RequestOptions.DEFAULT);
