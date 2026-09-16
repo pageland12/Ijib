b9848159f0d8ab86b846a9359cea0ed07f6f4948
CMS <choims2637@gmail.com>
2026-09-16 17:52:27 +0900

    09/16 17:52 CMS

--- src/main/resources/static/js/noticeWU.js
+++ src/main/resources/static/js/noticeWU.js
@@ -7,11 +7,5 @@ function check() {
 		return false;
 	}
 	
-	if(ntitle.value.length<2 || ntitle.value.length>15) {
-		alert("제목은 2자이상 15자 이하로 입력해주세요.");
-		ntitle.focus();
-		return false;
-	}
-	
 	return true;
 }
